f119f95366fec0635d237e9d50d9a417858facf1
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-08 15:50:19 -0300

    Herança

---
+++ src/_1_heranca/exemplo/application/Program.java
@@ -0,0 +1,13 @@
+package _1_heranca.exemplo.application;
+
+import _1_heranca.exemplo.entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+	}
+	
+}
